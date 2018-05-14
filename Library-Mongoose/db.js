0753182c1e5d805c0cf986b1faddde167e9d7564
DESKTOP-UPKUCKU\T9Academy <dxu@techtonicgroup.com>
2018-05-14 10:02:05 -0600

    start Library App with Mongoose no-sql Database and NPM

---
+++ Library-Mongoose/db.js
@@ -0,0 +1,4 @@
+// db.js
+var mongoose = require('mongoose');
+// mongoose.connect('{insert connection string from mlabs}');
+mongoose.connect('mongodb://davidxu:t3cht0n!c@ds119800.mlab.com:19800/techtonic');
